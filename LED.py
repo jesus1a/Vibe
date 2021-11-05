12444cbf13542f73113986f138e3128bd8c767a1
jesus1a <48657218+jesus1a@users.noreply.github.com>
2021-11-04 19:20:32 -0700

    Update LED.py

--- LED.py
+++ LED.py
@@ -1,1 +1,17 @@
+import RPi.GPIO as GPIO
+import time as time
+GPIO.cleanup()
+led_pin = 4
+def initialize():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(led_pin,GPIO.OUT)
 
+def main():
+    while True:
+        GPIO.output(led_pin,GPIO.HIGH)
+        time.sleep(5000)
+        GPIO.output(led_pin,GPIO.LOW)
+
+initialize()
+main()
+GPIO.cleanup()
