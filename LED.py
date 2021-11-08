6671d8d24107c015838a5eb51c648e932a412aa0
jesus1a <48657218+jesus1a@users.noreply.github.com>
2021-11-08 15:49:15 -0800

    Update LED.py

--- LED.py
+++ LED.py
@@ -1,15 +1,17 @@
 #So far this should just turn an LED on and off constantly in 5 sec intervals
 import RPi.GPIO as GPIO
 import time as time
-
-led_pin = 17 #port num will change on finall realese
-
-GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
 GPIO.setup(led_pin,GPIO.OUT)
 
-def on():
-    GPIO.output(led_pin,GPIO.HIGH)
-def off():
-    GPIO.output(led_pin,GPIO.LOW)
+
+def blink(detect, pin):
+    led_pin = pin
+    while(detect):
+        GPIO.output(led_pin,GPIO.HIGH)
+        time.sleep(.5)
+        GPIO.output(led_pin,GPIO.LOW)
+        time.sleep(.5)
 
 GPIO.cleanup()
