f5f022267e0e6c78da12e721826263713148fe3a
jesus1a <48657218+jesus1a@users.noreply.github.com>
2021-11-08 15:20:54 -0800

    Update LED.py
    eventually we will need to have just methods in this file and take in a port number in the definition

--- LED.py
+++ LED.py
@@ -1,20 +1,15 @@
 #So far this should just turn an LED on and off constantly in 5 sec intervals
 import RPi.GPIO as GPIO
 import time as time
-GPIO.cleanup()
-led_pin = 4
-def initialize():
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(led_pin,GPIO.OUT)
 
-#def main():
-#    while True:
-#        GPIO.output(led_pin,GPIO.HIGH)
-#        time.sleep(5000)
-#        GPIO.output(led_pin,GPIO.LOW)
+led_pin = 17 #port num will change on finall realese
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(led_pin,GPIO.OUT)
+
 def on():
     GPIO.output(led_pin,GPIO.HIGH)
 def off():
     GPIO.output(led_pin,GPIO.LOW)
-initialize()
+
 GPIO.cleanup()
