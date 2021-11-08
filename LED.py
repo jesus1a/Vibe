bce4aea6288b9c41c048373ee4c3d2fb2e794163
jesus1a <48657218+jesus1a@users.noreply.github.com>
2021-11-08 14:35:31 -0800

    Update LED.py

--- LED.py
+++ LED.py
@@ -7,12 +7,14 @@ def initialize():
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(led_pin,GPIO.OUT)
 
-def main():
-    while True:
-        GPIO.output(led_pin,GPIO.HIGH)
-        time.sleep(5000)
-        GPIO.output(led_pin,GPIO.LOW)
-
+#def main():
+#    while True:
+#        GPIO.output(led_pin,GPIO.HIGH)
+#        time.sleep(5000)
+#        GPIO.output(led_pin,GPIO.LOW)
+def on():
+    GPIO.output(led_pin,GPIO.HIGH)
+def off():
+    GPIO.output(led_pin,GPIO.LOW)
 initialize()
-main()
 GPIO.cleanup()
